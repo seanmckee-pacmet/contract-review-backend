6bd3deb7c445c4370e9fb1fd3a676fdceee84f25
seanmckee-pacmet <sean.mckee@pacmetaerospace.com>
2024-10-04 15:11:26 -0700

    changed origins for CORS

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Add your frontend URL
+    allow_origins=["http://localhost:3000", "https://contract-review-frontend.vercel.app"],  # Add your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
